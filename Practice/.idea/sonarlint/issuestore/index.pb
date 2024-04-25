0d7f0b49aa22d75ab02ac26b0605e6c7357dce1d
Nishant Kumar <nishant.kumar@trangile.net>
2024-04-25 15:49:43 +0530

    adding practice to repository

---
+++ Practice/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+K
+src/main/java/Practice.java,b\3\b31e0d904a1b32ba58efc4d290a64a6b259599ed
---
+++ Practice/src/main/java/Practice.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Practice {
+
+    ArrayList<Integer> list=new ArrayList<>();
+
+
+
+}
